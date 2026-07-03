bee9961341e1ff5741901c1f8ff8357456a478f1
Vinicius Fonseca <viniciusfonseca.alcans@gmail.com>
2026-07-02 22:27:18 -0300

    chore: remove arquivos da estrutura antiga de controllers e repositorios substituidos pela organizacao por dominio

--- src/http/routes.ts
+++
@@ -1,8 +0,0 @@
-import type { FastifyInstance } from "fastify";
-import { register } from "./controllers/register.controller.js";
-import { authenticate } from "./controllers/authenticate.controller.js";
-
-export async function appRoutes(app: FastifyInstance) {
-  app.post("/users", register);
-  app.post("/sessions", authenticate);
-}
