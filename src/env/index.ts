5d27069be41af79c9d0aeb87ebac8dad9321add2
Vinicius Fonseca <viniciusfonseca.alcans@gmail.com>
2026-07-02 22:19:56 -0300

    chore: adiciona validacao de variaveis de ambiente com Zod

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,9 @@ import { z } from "zod";
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string()
 });
 
 const _env = envSchema.safeParse(process.env);
